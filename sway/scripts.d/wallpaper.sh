0ba824a7e5050dfd6f27bd520a5335d14bf1f44d
erffy <ceyhunerenerguven.07@outlook.com>
2024-07-29 14:55:35 +0300

    29072024

--- sway/scripts.d/wallpaper.sh
+++ sway/scripts.d/wallpaper.sh
@@ -5,40 +5,48 @@ INTERVAL=60
 TRANSITION_TIME=1
 PID_FILE="/tmp/swaybg.pid"
 USED_WALLPAPERS=()
+PREV_WALLPAPER=""
 
 # Function to start swaybg with the given wallpaper
 start_swaybg() {
-  swaybg -m fill -i $1 &
-  echo $! > "$PID_FILE"
+  local wallpaper=$1
+  swaybg -m fill -i $wallpaper &
+  echo $! > $PID_FILE
 }
 
 # Function to stop swaybg
 stop_swaybg() {
   if [ -f "$PID_FILE" ]; then
-    kill $(cat $PID_FILE)
+    kill $(cat $PID_FILE) 2>/dev/null || true
     rm $PID_FILE
   fi
 }
 
 # Function to check if a wallpaper is already used
 contains() {
-  local e
-  for e in "${USED_WALLPAPERS[@]}"; do
-    [[ "$e" == "$1" ]] && return 0
+  local wallpaper=$1
+  for used in "${USED_WALLPAPERS[@]}"; do
+    [[ "$used" == "$wallpaper" ]] && return 0
   done
   return 1
 }
 
 # Get list of all wallpapers
-all_wallpapers=($(find $WALLPAPER_DIR -type f))
+all_wallpapers=($(find "$WALLPAPER_DIR" -type f))
+
+if [ ${#all_wallpapers[@]} -eq 0 ]; then
+  echo "No wallpapers found in $WALLPAPER_DIR"
+  exit 1
+fi
 
 while true; do
   # Refresh list of used wallpapers if all wallpapers have been used
-  if [[ ${#USED_WALLPAPERS[@]} -ge ${#all_wallpapers[@]} ]]; then
+  if [ ${#USED_WALLPAPERS[@]} -ge ${#all_wallpapers[@]} ]; then
     USED_WALLPAPERS=()
   fi
 
   # Select a random wallpaper that hasn't been used
+  local WALLPAPER
   while true; do
     WALLPAPER="${all_wallpapers[RANDOM % ${#all_wallpapers[@]}]}"
     if [[ "$WALLPAPER" != "$PREV_WALLPAPER" ]] && ! contains "$WALLPAPER"; then
@@ -46,14 +54,18 @@ while true; do
     fi
   done
 
-  # Start new wallpaper and transition
-  start_swaybg $WALLPAPER
-  sleep $TRANSITION_TIME
-
+  # Stop the previous wallpaper
   stop_swaybg
 
+  # Start new wallpaper
+  start_swaybg $WALLPAPER
+
+  # Ensure there's enough time for the new wallpaper to be fully applied
+  sleep $TRANSITION_TIME
+  
+  # Update the list of used wallpapers
   USED_WALLPAPERS+=("$WALLPAPER")
-  PREV_WALLPAPER=$WALLPAPER
+  PREV_WALLPAPER="$WALLPAPER"
 
   sleep $INTERVAL
 done
