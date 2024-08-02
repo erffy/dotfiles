c285398755706ab5a347cc071a9d93f08c02fad1
erffy <ceyhunerenerguven.07@outlook.com>
2024-08-02 21:33:45 +0300

    02082024

--- waybar/scripts/ping.sh
+++ waybar/scripts/ping.sh
@@ -1,26 +1,13 @@
 #!/usr/bin/env bash
 
-# Target for ping
 TARGET="google.com"
+DNS=$(echo $(grep '^nameserver' /etc/resolv.conf | awk '{print $2}') | awk '{print "DNS" NR ": " $1}' | tr '\n' '\n')
 
-# Function to perform the ping and extract latency
-get_ping_time() {
-  local ping_output
-  local ping_time
+ping_output=$(ping -c 1 $TARGET 2>/dev/null)
+ping_time=$(echo $ping_output | awk -F 'time=' '/time=/ {print $2}' | awk '{print $1}')
 
-  # Run the ping command and capture the output
-  ping_output=$(ping -c 1 "$TARGET" 2>/dev/null)
-
-  # Check if the ping command was successful
-  if [[ $? -eq 0 ]]; then
-    # Extract the ping time using awk for cleaner processing
-    ping_time=$(echo "$ping_output" | awk -F 'time=' '/time=/ {print $2}' | awk '{print $1}')
-    echo "${ping_time}ms"
-  else
-    # If ping fails, return a empty output
-    echo ""
-  fi
-}
-
-# Call the function and output the result
-get_ping_time
+if [[ $? -eq 0 ]]; then
+  echo "{\"text\":\"   ${ping_time}ms\", \"tooltip\":\"Target: ${TARGET}\n${DNS}\"}"
+else
+  echo "{\"text\":\"\", \"tooltip\":\"\", \"class\":\"hidden\"}"
+fi
